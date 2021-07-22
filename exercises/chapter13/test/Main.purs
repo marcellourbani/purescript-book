bc8631d03c03c327a4b7521da4f855a215ca4f31
Marcello Urbani <marcello.urbani@gmail.com>
2021-07-22 08:56:50 +0100

    chapter 13

--- exercises/chapter13/test/Main.purs
+++ exercises/chapter13/test/Main.purs
@@ -2,15 +2,22 @@ module Test.Main where
 
 import Prelude
 
-import Data.Array (sort, sortBy)
+import Control.Monad.List.Trans (foldl)
+import Data.Array (fold, sort, sortBy, (..))
+import Data.Array.NonEmpty (NonEmptyArray(..), range, singleton)
+import Data.Char (fromCharCode, toCharCode)
 import Data.Foldable (foldr)
 import Data.Function (on)
 import Data.List (List(..), fromFoldable)
+import Data.Maybe (fromMaybe)
+import Data.Traversable (traverse)
 import Effect (Effect)
 import Merge (mergeWith, mergePoly, merge)
 import Sorted (sorted)
 import Test.QuickCheck (quickCheck, (<?>))
-import Tree (Tree, member, insert, toArray, anywhere)
+import Test.QuickCheck.Arbitrary (arbitrary, class Arbitrary)
+import Test.QuickCheck.Gen (Gen, arrayOf, elements)
+import Tree (Tree, anywhere, insert, member, toArray)
 
 isSorted :: forall a. (Ord a) => Array a -> Boolean
 isSorted = go <<< fromFoldable
@@ -21,11 +28,26 @@ isSorted = go <<< fromFoldable
 ints :: Array Int -> Array Int
 ints = identity
 
+bools :: Array Boolean -> Array Boolean
+bools = identity
+
+strings :: Array String -> Array String
+strings = identity
+
 intToBool :: (Int -> Boolean) -> Int -> Boolean
 intToBool = identity
 
 treeOfInt :: Tree Int -> Tree Int
 treeOfInt = identity
+
+newtype AzString = AzString String
+
+instance arbitraryAz :: Arbitrary AzString where
+  arbitrary:: Gen AzString
+  arbitrary = 
+    let chars = fromMaybe (singleton 'a') $ traverse fromCharCode $ range (toCharCode 'a') (toCharCode 'z')
+        createAz = map show >>> fold >>> AzString
+    in createAz <$> (arrayOf $ elements chars)
 
 main :: Effect Unit
 main = do
@@ -38,24 +60,63 @@ main = do
     in
       eq result expected <?> "Result:\n" <> show result <> "\nnot equal to expected:\n" <> show expected
 
-  quickCheck \xs ys ->
-    eq (merge (sorted xs) (sorted ys)) (sort $ sorted xs <> sorted ys)
+  quickCheck \xs ->
+    let
+      result = merge xs []
+      expected = xs
+    in
+      eq result expected <?> "Result:\n" <> show result <> "\nnot equal to expected:\n" <> show expected
 
   quickCheck \xs ys ->
-    eq (ints $ mergePoly (sorted xs) (sorted ys)) (sort $ sorted xs <> sorted ys)
+    let 
+      result = merge (sorted xs) (sorted ys)
+      expected = sort $ sorted xs <> sorted ys
+    in
+      eq result expected <?> "Result:\n" <> show result <> "\nnot equal to expected:\n" <> show expected
 
+  quickCheck \xs ys ->
+    let 
+      result = ints $ mergePoly (sorted xs) (sorted ys)
+      expected = sort $ sorted xs <> sorted ys
+    in
+      eq result expected <?> "Result:\n" <> show result <> "\nnot equal to expected:\n" <> show expected
+  
+
+  quickCheck \xs ys ->
+    let 
+      result = bools $ mergePoly (sorted xs) (sorted ys)
+      expected = sort $ sorted xs <> sorted ys
+    in
+      eq result expected <?> "Result:\n" <> show result <> "\nnot equal to expected:\n" <> show expected
+            
   quickCheck \xs ys f ->
     let
       result = map f $ mergeWith (intToBool f) (sortBy (compare `on` f) xs) (sortBy (compare `on` f) ys)
       expected = map f $ sortBy (compare `on` f) $ xs <> ys
     in
-      eq result expected
+      eq result expected <?> "Result:\n" <> show result <> "\nnot equal to expected:\n" <> show expected
 
   -- Tests for module 'Tree'
 
-  quickCheck \t a -> member a $ insert a $ treeOfInt t
-  quickCheck \t xs -> isSorted $ toArray $ foldr insert t $ ints xs
+  quickCheck \t a -> (member a $ insert a $ treeOfInt t) <?> "Inserted value:\n" <> show a <> "\nnot found in tree\n"
+    
+
+  quickCheck \t xs -> (isSorted $ toArray $ foldr insert t $ ints xs) <?> "Folding insert should return a sorted array\n" <> show xs
 
   quickCheck \f g t ->
     anywhere (\s -> f s || g s) t ==
       anywhere f (treeOfInt t) || anywhere g t
+
+  -- exercise: quickcheck for fold
+  quickCheck \xs ys ->
+    let result = strings $ fold $ xs <> ys
+        expected = fold xs <> fold ys
+    in 
+      eq result expected <?> "Result:\n" <> show result <> "\nnot equal to expected:\n" <> show expected
+
+  -- exercise: quickcheck for tree insertions
+  quickCheck \t a bs ->
+    let ta = insert a t
+        tb = foldr insert ta (ints bs)
+    in member a tb <?> "Inserted value:\n" <> show a <> "\nnot found in tree after inserting\n" <> show bs
+    
